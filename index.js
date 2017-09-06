aa3cc4a9f4fffdeaa1eb82dc54ad8f47c644747c
Robin <robincher@gmail.com>
2017-09-06 14:49:59 +0800

    Fix whitespace for index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 module.exports = {
     ApiSecurityUtil: require('./lib/ApiSecurityUtil'),
-    ApexAuthorizationUtil : require('./lib/ApexAuthorizationUtil ')
+    ApexAuthorizationUtil : require('./lib/ApexAuthorizationUtil')
 };

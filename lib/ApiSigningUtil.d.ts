930790232fb6e93e0d51dae982f4132b3f382685
Robin Cher <robin_cher@ida-gds.com>
2017-11-30 18:40:32 +0800

    #9 | Update typescript definition to include new function

--- lib/ApiSigningUtil.d.ts
+++ lib/ApiSigningUtil.d.ts
@@ -8,10 +8,12 @@ export declare function getPrivateKeyFromPem(pemFileName: string);
 export declare function getPublicKeyFromCer(cerFileName: string);
 
 export declare function getBaseString(authPrefix: string, signatureMethod: string, appId: string, urlPath: string, httpMethod: string, formData?: object, nonce: string, timestamp: number);
+export declare function getSignatureBaseString(baseProps: object);
 
 export declare function getTokenFromSecret(realm: string, authPrefix: string, httpMethod: string, urlPath: string, appId: string, secret: string, formJson?: object, nonce?: string, timestamp?: number);
 export declare function getTokenFromCertFileName(realm: string, authPrefix: string, httpMethod: string, urlPath: string, appId: string, formJson?: string, passphrase: string, certFileName: string, nonce?: string, timestamp?: number);
 export declare function getTokenFromCertString(realm: string, authPrefix: string, httpMethod: string, urlPath: string, appId: string, formJson?: object, passphrase: string, certString: string, nonce?: string, timestamp?: number);
 export declare function getToken(realm: string, authPrefix: string, httpMethod: string, urlPath: string, appId: string, secret?: string, formJson?: object, passphrase?: string, certFileName?: string, nonce?: string, timestamp?: number, certString?: string);
+export declare function getSignatureToken(reqProps: object);
 
 export declare function makeHttpRequest(urlPath: string, token: string, formData?: object, httpMethod: string, port: number);

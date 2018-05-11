21062fef913fc772dc83f65dafb97e81bc8a2cde
Wei Junyu <junyuwei@gmail.com>
2018-05-11 11:17:37 +0800

    Update for publishing

--- lib/ApiSigningUtil.d.ts
+++ lib/ApiSigningUtil.d.ts
@@ -1,12 +1,10 @@
-export declare function setLogLevel(loglevel: string);
+export declare function getHMACSignature(message: string , secret: string);
 
-export declare function getHMACSignature(message: string | Buffer, secret: string | Buffer);
+export declare function verifyHMACSignature(signature: string, secret: string, message: string);
 
-export declare function verifyHMACSignature(signature: string, secret: string | Buffer, message: string | Buffer);
+export declare function getRSASignature(message: string, privateKey: string, passphrase: string);
 
-export declare function getRSASignature(message: string | Buffer, privateKey: string, passphrase: string);
-
-export declare function verifyRSASignature(signature: string, publicKey: string, message: string | Buffer);
+export declare function verifyRSASignature(signature: string, publicKey: string, message: string);
 
 export declare function getPrivateKeyFromPem(pemFileName: string);
 
@@ -14,14 +12,4 @@ export declare function getPublicKeyFromCer(cerFileName: string);
 
 export declare function getSignatureBaseString(baseProps: object);
 
-export declare function getTokenFromSecret(realm: string, authPrefix: string, httpMethod: string, urlPath: string, appId: string, secret: string, formJson?: object, nonce?: string, timestamp?: number);
-
-export declare function getTokenFromCertFileName(realm: string, authPrefix: string, httpMethod: string, urlPath: string, appId: string, formJson?: string, passphrase: string, certFileName: string, nonce?: string, timestamp?: number);
-
-export declare function getTokenFromCertString(realm: string, authPrefix: string, httpMethod: string, urlPath: string, appId: string, formJson?: object, passphrase: string, certString: string, nonce?: string, timestamp?: number);
-
-export declare function getToken(realm: string, authPrefix: string, httpMethod: string, urlPath: string, appId: string, secret?: string, formJson?: object, passphrase?: string, certFileName?: string, nonce?: string, timestamp?: number, certString?: string);
-
 export declare function getSignatureToken(reqProps: object);
-
-export declare function sendRequest(urlPath: string, token: string, formData?: object, httpMethod: string, port: number);

9f49d791dbd9d9c080163286a6ce8f62727bb612
Wei Junyu <junyuwei@gmail.com>
2018-10-19 11:40:39 +0800

    Updated eslint config and linting.

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "parserOptions": {
+        "ecmaVersion": 2015
+    },
+    "plugins": ["security"],
+    "extends": ["eslint:recommended", "plugin:security/recommended"],
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,9 @@ All notable changes to this project will be documented in this file.
 
 This project adheres to [Semantic Versioning](http://semver.org/).
 
+### V1.0.2
++ Eslint config and linting for library
+
 ### V1.0.0
 + Bump to version 1.0.0 major release candidate
 + Removed deprecated interfaces getTokenFromSecret, getTokenFromCertFileName and getTokenFromCertString
